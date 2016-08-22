8ec9170fee1679f2ff27d398d5555539a69cf243
jaspercurry <jaspercurry@gmail.com>
2016-08-21 22:52:49 -0400

    made lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+
+
   end
 
   def index
@@ -9,8 +11,34 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(safe_params)
+    @recipe.save
+    redirect_to @recipe
   end
+
+private
+
+  def safe_params
+
+    p = params.require(:recipe).permit(:title, ingredients_attributes: [ :name, :quantity ])
+    p[:ingredients_attributes].delete_if { |k, v| v[:name] == ""}
+    p
+
+  end
+
+
+
 end
